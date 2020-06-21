35408f19c75c35b25aed25e68458beda031f90b8
Sathish kumar D <sathishkumard_22@yahoo.co.in>
2020-06-21 21:24:57 +0400

    model added; model fields added

--- teachers_dir/directory/models.py
+++ teachers_dir/directory/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Teacher(models.Model):
+    First_Name = models.CharField(max_length=60)
+    Last_Name = models.CharField(max_length=60)
+    Profile_Picture = models.CharField(max_length=50)
+    Email_Address = models.EmailField(max_length = 254) 
+    Phone_Number = models.CharField(max_length=31)
+    Room_Number = models.CharField(max_length=10)
+    #Subjects Taught
